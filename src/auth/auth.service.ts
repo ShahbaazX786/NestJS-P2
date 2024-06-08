06c688f3edcbded8934151a7be3731f5a73b8cfe
ShahbaazX786 <shahbaazplatinum@gmail.com>
2024-06-08 13:45:07 +0530

    Updated auth service a lil bit

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import { createUserDto } from 'src/user/dto/createUser.dto';
+import { UserService } from 'src/user/user.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private user: UserService) {}
+
+  async validateUser(email, password) {
+    const users = await this.user.findUserByEmail(email);
+    if (users && users.length > 1) {
+      let response:any = 1;
+      const user = users.find((user) => user.password === password);
+      if (user) {
+        response = user;
+      }
+      return response;
+    } else {
+      return 2;
+    }
+  }
+
+  async createUser(req: createUserDto) {
+    return await this.user.createUser(req);
+  }
+}
