5bb8f6e49f4357bddb98d794a0bb2df6785ddf39
ShahbaazX786 <shahbaazplatinum@gmail.com>
2024-06-08 13:46:03 +0530

    Updated login controller to use passport guard directly instead of service.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,28 +1,20 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { UserService } from 'src/user/user.service';
-import { loginDto } from './dto/login.dto';
+import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { createUserDto } from 'src/user/dto/createUser.dto';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private user: UserService) {}
+  constructor(private auth: AuthService) {}
+
+  @UseGuards(AuthGuard('local'))
   @Post('/login')
-  async login(@Body() req: loginDto) {
-    const users = await this.user.findUserByEmail(req.email);
-    if (users && users.length > 1) {
-      let response = 'Password Does not match';
-      const user = users.find((user) => user.password === req.password);
-      if (user) {
-        response = 'Login Sucessful';
-      }
-      return response;
-    } else {
-      return 'Please check your email';
-    }
+  async login(@Request() req: any) {
+    return req.user;
   }
 
   @Post('/signup')
-  signup(@Body() req: createUserDto) {
-    this.user.createUser(req);
+  async signup(@Body() req: createUserDto) {
+    return await this.auth.createUser(req);
   }
 }
