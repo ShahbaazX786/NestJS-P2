3769f5c023c76082becaecb1e479790bdb744759
ShahbaazX786 <shahbaazplatinum@gmail.com>
2024-06-08 14:46:04 +0530

    injected jwt service and used in auth service.

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { createUserDto } from 'src/user/dto/createUser.dto';
 import { UserService } from 'src/user/user.service';
+import { loginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
-  constructor(private user: UserService) {}
+  constructor(
+    private user: UserService,
+    private jwt: JwtService,
+  ) {}
 
   async validateUser(email, password) {
     const users = await this.user.findUserByEmail(email);
     if (users && users.length > 1) {
-      let response:any = 1;
+      let response: any = 1;
       const user = users.find((user) => user.password === password);
       if (user) {
         response = user;
@@ -22,5 +27,12 @@ export class AuthService {
 
   async createUser(req: createUserDto) {
     return await this.user.createUser(req);
+  }
+
+  async login(user: any) {
+    const payload = { email: user.email, sub: user.id };
+    return {
+      access_token: this.jwt.sign(payload),
+    }
   }
 }
