6bc4c111172f079d00c7ccba776c034551d069c0
ShahbaazX786 <shahbaazplatinum@gmail.com>
2024-06-08 11:58:23 +0530

    Added and configured typeorm for appmodule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/entity/user.entity';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '127.0.0.1',
+      port: 3306,
+      username: 'root',
+      password: 'password',
+      database: 'nest2',
+      entities: [User],
+      synchronize: true, // disble this in build
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
